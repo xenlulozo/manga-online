8bf63c6f7a5c7a9f8a5e39bb4d18c8f1765eb382
hieu <baymaxvipx@gmail.com>
2023-07-13 07:11:52 +0700

    fix deploy 11

---
+++ src/app/watch/[...slug]/base.tsx
@@ -0,0 +1,37 @@
+"use client"
+import 'bootstrap/dist/css/bootstrap.css';
+import React, { useEffect, useState } from 'react';
+import getImageNames from "../../getImages"
+import Image from 'next/image';
+import getPublicDirectories from '@/app/countDirectories';
+import Content from '@/app/watch/[...slug]/content';
+import { data } from 'autoprefixer';
+
+interface ComponentAProps {
+  index: number
+}
+export default function Base(slug: any) {
+  const [imageNames, setImageNames] = useState([]);
+
+  useEffect(() => {
+    // console.log(slug && slug.slug)
+    const fetchImageNames = async () => {
+
+      try {
+        const response = await fetch(`http://localhost:3000/api/getImage?name=${slug.slug[0]}&chap=${slug.slug[1]}`);
+
+        const data = await response.json();
+        console.log(data)
+
+        setImageNames(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchImageNames();
+  }, []);
+  return <>
+    <Content slug={imageNames} name={slug.slug[0]} chap={slug.slug[1]} />
+  </>
+}
--- src/app/watch/[...slug]/page.tsx
+++ src/app/watch/[...slug]/page.tsx
@@ -4,26 +4,26 @@ import getImageNames from "../../getImages"
 import Content from './content';
 import NextPrev from './fromClient';
 import getPublicDirectories from '@/app/countDirectories';
-
+import Base from './base';
 interface ComponentAProps {
   index: number
 }
-export default async function Page({ params }: { params: { slug: string, index: Number } }) {
-  const publicDirectories = getPublicDirectories(params.slug[0
-  ]);
-  const imageNames: string[] = getImageNames(params.slug);
+export default async function Page({ params }: { params: { slug: string } }) {
+  // const publicDirectories = getPublicDirectories(params.slug[0
+  // ]);
+  // const imageNames: string[] = getImageNames(params.slug);
 
   return <>
-    {/* {console.log("go")} */}
+    {/* {console.log(params.slug)} */}
     <div className="container">
 
       <div className='justify-content-center'>
 
-        <Content slug={imageNames} name={params.slug[0]} chap={params.slug[1]} />
-
+        {/* <Content slug={imageNames} name={params.slug[0]} chap={params.slug[1]} /> */}
+        <Base slug={params.slug} />
       </div>
 
-      <NextPrev slug={params.slug} indexSV={publicDirectories.length} />
+      {/* <NextPrev slug={params.slug} indexSV={publicDirectories.length} /> */}
 
 
 
